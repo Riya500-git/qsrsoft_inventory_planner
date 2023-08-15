cb996780dbda9280cce600ad503070a08bdde432
Steve Wang <stevewang@regroup.app>
2023-08-15 10:34:45 -0700

    Integrate endpoint into website

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,80 +1,4 @@
-// import React, {useState} from 'react';
-// import { Box, Center, Text, Input, Button, ChakraProvider, Select } from '@chakra-ui/react';
-
-// function MainPage() {
-//   const [latitude, setLatitude] = useState('');
-//   const [longitude, setLongitude] = useState('');
-
-//   const handleRecommend = async () => {
-//     try {
-//     } catch (error) {
-//       console.error('Error', error);
-//     }
-//   };
-
-//   return (
-//     <ChakraProvider>
-
-//       <Box
-//         bgGradient="linear(to-b, #f3f3f3, #ffffff)"
-//         p={8}
-//         borderRadius="lg"
-//         boxShadow="xl"
-//         width="400px"
-//       >
-//         <Box display="flex" justifyContent="space-between" alignItems="center">
-//         <img src="logo.png" alt="CloudX Hackathon" />
-//           <Text fontSize="xl">Company Name</Text>
-//           <Box>
-//             <Button variant="ghost" colorScheme="blue" mr={2}>
-//               Button 1
-//             </Button>
-//             <Button variant="ghost" colorScheme="blue">
-//               Button 2
-//             </Button>
-//           </Box>
-//         </Box>  
-//           <Text mt={4} fontSize="lg" textAlign="left">Franchise</Text>
-//           <Select mt={2} placeholder="Select an option">
-//             <option>Option 1</option>
-//             <option>Option 2</option>
-//             <option>Option 3</option>
-//           </Select>
-//           {/* <Text mt={4} textAlign="left">Country</Text> */}
-//           {/* <Input type="text" mt={2} placeholder="Search and select a country" /> */}
-//           {/* <Select mt={2} placeholder="Select a country">
-//             {countries.map((country, index) => (
-//               <option key={index}>{country}</option>
-//             ))}
-//           </Select> */}
-//           <Text mt={4} fontSize="lg" textAlign="left">Latitude</Text>
-//           <Box display="flex">
-//           <Input
-//             type="text"
-//             mt={2}
-//             placeholder="Enter latitude"
-//             value={latitude}
-//             onChange={(e) => setLatitude(e.target.value)}
-//             marginRight={2}
-//           />
-//           <Text mt={2} textAlign="left">Longitude</Text>
-//           <Input
-//             type="text"
-//             mt={2}
-//             placeholder="Enter longitude"
-//             value={longitude}
-//             onChange={(e) => setLongitude(e.target.value)}
-//           />
-//           <Button mt={4} colorScheme="blue" onClick={handleRecommend} width="100%">Recommend</Button>
-//         </Box>
-//         </Box>
-//     </ChakraProvider>
-//   );
-// }
-
-// export default MainPage;
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   Text,
@@ -91,12 +15,19 @@ import Typewriter from 'typewriter-effect';
 function MainPage() {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [expectedDate, setExpectedDate] = useState('');
+  const [output, setOutput] = useState('');
 
   const handleRecommend = async () => {
-    try {
-    } catch (error) {
-      console.error('Error', error);
+    if (!latitude || !longitude) {
+      setOutput("Invalid inputs");
+      return;
     }
+    fetch("http://127.0.0.1:5000/inventory?location_id=1&item_id=1&week=1&month=1", {mode:'cors'})
+      .then((response) => response.json())
+      .then((json) => {
+        setOutput(json);
+      });
   };
 
   return (
@@ -105,7 +36,7 @@ function MainPage() {
         <Box p={4} borderRadius="lg" boxShadow="md" bg="green.700">
           <Flex justify="space-between" align="center" mb={4}>
             <Text fontSize="xl" fontWeight="bold" color="white">
-              QRSoft Inventory Planner
+              QSRSoft Inventory Planner
             </Text>
             <Flex>
               <Button colorScheme="white" mr={2}>
@@ -161,6 +92,10 @@ function MainPage() {
           <Button mt={6} colorScheme="green" onClick={handleRecommend} width="300px">
             Recommend
           </Button>
+
+          <Text fontSize="lg" textAlign="left" mr={2} color="green.800">
+            {output}
+          </Text>
 
           <Text mt={6} fontSize="xl" color="green.800" fontWeight="bold">
             <Typewriter
